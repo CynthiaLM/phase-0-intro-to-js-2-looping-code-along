044b1cc1b84a65dccbab947e2ca4b6fa2b1937d6
Laleti Cynthia <+>
2023-07-21 16:40:36 +0300

    Completed this lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const cards = ["Guadalupe","Ollie","Aki"]
+function writeCards(cards, eventName) {
+  let messages = [];
+  for (let i = 0; i < cards.length; i++) {
+    messages.push(`Thank you, ${cards[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return messages;
+}
+
+function countDown() {
+  let i=10;
+  while(i>=0){
+    console.log(i--);
+  }
+}
